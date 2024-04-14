d34b13e5c3efdb85e243af444904768b704863f4
joaovitorlopes <jvlaaa+github@gmail.com>
2024-04-14 00:39:33 -0400

    adding feature of show client data

---
+++ src/Challenge.java
@@ -0,0 +1,12 @@
+public class Challenge {
+    public static void main(String[] args) {
+        String name = "Fulano de Tal";
+        String accountType = "Current Account";
+        double balance = 2000;
+        System.out.println("******************************");
+        System.out.printf("\nClient Name: %s%n", name);
+        System.out.printf("Account Type: %s%n", accountType);
+        System.out.printf("Current Balance: R$%.2f%n", balance);
+        System.out.println("\n******************************");
+    }
+}
